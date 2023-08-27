a522afc3920d50fff342c798ad1e40ac1bb07911
Arya27Ambagade <arya.ambagade@cumminscollege.edu.in>
2023-08-27 13:49:49 +0530

    Panelist Dashboard Changes

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -4,6 +4,7 @@ import Footer from "../components/Footer";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import config from "./config";
+import { NavLink } from "react-router-dom";
 const apiUrl = `${config.backendUrl}/dashboard/`; // Construct Backend API URL
 
 export const Dashboard = () => {
@@ -56,6 +57,14 @@ export const Dashboard = () => {
                   </div>
                   <div className='col-4'>
                     <h4>User Type: {userDetails.user_type}</h4>
+                  </div>
+                </div>
+                <div className='row mt-4'>
+                  <div className='col-6'>
+                  <NavLink to='/IdeaEvaluation' className="btn btn-style-one"> <span>Evaluated Ideas</span> </NavLink>
+                  </div>
+                  <div className='col-6'>
+                  <NavLink to='/IdeaEvaluation' className="btn btn-style-one"> <span>Start Evaluation</span> </NavLink>
                   </div>
                 </div>
               </>
