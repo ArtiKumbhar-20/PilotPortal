22a5313a1a32b4fa622634408ad13635f4002572
Harvi KevinKumar Patel <101132485+Harviiii@users.noreply.github.com>
2024-01-06 18:54:21 +0530

    Quiz Section

--- frontend/src/components/QuizPage.js
+++ frontend/src/components/QuizPage.js
@@ -1,104 +1,4 @@
-// import React, { useState } from "react";
-// import "./Quiz.css";
 
-// function QuizPage() {
-//   const questions = [
-//     {
-//       questionText: "What is the capital of France?",
-//       answerOptions: [
-//         { answerText: "1) New York", isCorrect: false },
-//         { answerText: "2) London", isCorrect: false },
-//         { answerText: "3) Paris", isCorrect: true },
-//         { answerText: "4) Dublin", isCorrect: false },
-//       ],
-//     },
-//     {
-//       questionText: "Who is CEO of Tesla?",
-//       answerOptions: [
-//         { answerText: "1) Jeff Bezos", isCorrect: false },
-//         { answerText: "2) Elon Musk", isCorrect: true },
-//         { answerText: "3) Bill Gates", isCorrect: false },
-//         { answerText: "4) Tony Stark", isCorrect: false },
-//       ],
-//     },
-//     {
-//       questionText: "The iPhone was created by which company?",
-//       answerOptions: [
-//         { answerText: "1) Apple", isCorrect: true },
-//         { answerText: "2) Intel", isCorrect: false },
-//         { answerText: "3) Amazon", isCorrect: false },
-//         { answerText: "4) Microsoft", isCorrect: false },
-//       ],
-//     },
-//     {
-//       questionText: "How many Harry Potter books are there?",
-//       answerOptions: [
-//         { answerText: "1) 1", isCorrect: false },
-//         { answerText: "2) 4", isCorrect: false },
-//         { answerText: "3) 6", isCorrect: false },
-//         { answerText: "4) 7", isCorrect: true },
-//       ],
-//     },
-//   ];
-
-//   const [currentQuestion, setCurrentQuestion] = useState(0);
-//   const [showScore, setShowScore] = useState(false);
-//   const [score, setScore] = useState(0);
-
-//   const handleAnswerOptionClick = (isCorrect) => {
-//     if (isCorrect) {
-//       setScore(score + 1);
-//     }
-
-//     const nextQuestion = currentQuestion + 1;
-//     if (nextQuestion < questions.length) {
-//       setCurrentQuestion(nextQuestion);
-//     } else {
-//       setShowScore(true);
-//     }
-//   };
-
-//   return (
-//     <div className='SpaceTop'>
-//       <div className='app'>
-//         {showScore ? (
-//           <div className='score-section'>
-//             You scored {score} out of {questions.length}
-//           </div>
-//         ) : (
-//           <>
-//             <div className='question-section'>
-//               <div className='question-count'>
-//                 Question {currentQuestion + 1}/{questions.length}
-//               </div>
-//               <div className='question-text title'>
-//                 {questions[currentQuestion].questionText}
-//               </div>
-//             </div>
-
-//             <div className='answer-section'>
-//               {questions[currentQuestion].answerOptions.map(
-//                 (answerOption, index) => (
-//                   <button
-//                     key={index}
-//                     onClick={() =>
-//                       handleAnswerOptionClick(answerOption.isCorrect)
-//                     }
-//                     className='answer-button'
-//                   >
-//                     {answerOption.answerText}
-//                   </button>
-//                 )
-//               )}
-//             </div>
-//           </>
-//         )}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default QuizPage;
 
 import { useState } from 'react'
 import { quiz } from './questions'
@@ -157,7 +57,7 @@ const QuizPage = () => {
             <span className="active-question-no">{addLeadingZero(activeQuestion + 1)}</span>
             <span className="total-question">/{addLeadingZero(questions.length)}</span>
           </div>
-          <h2>{question}</h2>
+          <h2 className="question">{question}</h2>
           <ul>
             {choices.map((answer, index) => (
               <li
@@ -174,21 +74,37 @@ const QuizPage = () => {
             </button>
           </div>
         </div>
+        
       ) : (
+       
         <div className="result">
-          <h3>Result</h3>
-          <p>
-            Total Question: <span>{questions.length}</span>
+
+          <h3 className="result-heading">Result</h3>
+
+          <div className="box-result">
+          <p className="marks">
+            Total Question: <span className="score">{questions.length}</span>
           </p>
-          <p>
-            Total Score:<span> {result.score}</span>
+          </div>
+
+          
+          <div className="box-result">
+          <p className="marks">
+            Total Score:<span className="score"> {result.score}</span>
           </p>
-          <p>
-            Correct Answers:<span> {result.correctAnswers}</span>
+          </div>
+
+          <div className="box-result">
+          <p className="marks">
+            Correct Answers:<span className="score"> {result.correctAnswers}</span>
           </p>
-          <p>
-            Wrong Answers:<span> {result.wrongAnswers}</span>
+          </div>
+
+          <div className="box-result">
+          <p className="marks">
+            Wrong Answers:<span className="score"> {result.wrongAnswers}</span>
           </p>
+          </div>
         </div>
       )}
     </div>
