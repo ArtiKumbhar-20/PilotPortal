3df57b83dd504983e28aca4e777c549230cf68f3
Gaurav Borse <gborse108@gmail.com>
2024-01-11 18:50:20 +0530

    Team Updated code.

--- frontend/src/components/TeamForm.js
+++ frontend/src/components/TeamForm.js
@@ -76,11 +76,12 @@ export const TeamForm = () => {
           teamForm.current.reset();
         } else if (response.status === 409) {
           setAlertType("danger");
-          setAlertMessage(response.data.error || "Team creation failed. Please check the form data.");
-        } else {
-          setAlertType("danger");
-          setAlertMessage("Else: Error submitting form. Please try again.");
+          setAlertMessage(response.data.error);
         }
+        // } else {
+        //   setAlertType("danger");
+        //   setAlertMessage("Else: Error submitting form. Please try again.");
+        // }
       } catch (error) {
         console.error("Error submitting form:", error);
 
