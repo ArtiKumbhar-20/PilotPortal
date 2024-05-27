ec22fb1c4445a4c8d87019f9feb79bd4b2f66791
Riya Thopate <109356995+riyathopate@users.noreply.github.com>
2024-05-27 23:34:52 +0530

    Backend Option for Partners

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path('', include('idea.urls')),
     path('', include ('event.urls')),
     path('', include ('quiz.urls')),
+    path('', include ('partner.urls')),
     path('token/', jwt_views.TokenObtainPairView.as_view(), name ='token_obtain_pair'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name ='token_refresh'),
     path('api/', include('quiz.urls')),
